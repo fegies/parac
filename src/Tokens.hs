a290a22045591ac47a81e86f774f2a4af49ba441
Felix Giese <s4fegies@uni-trier.de>
2017-03-26 22:43:04 +0200

    add tokens

---
+++ src/Tokens.hs
@@ -0,0 +1,49 @@
+module Tokens where
+
+type LexToken = (LexerPosition,Token)
+
+type LexerPosition = (
+    Int, --absolute offset
+    Int, --line
+    Int --column
+    )
+
+data Token
+    = TokenIf -- Keywords
+    | TokenElse
+    | TokenWhile
+    | TokenFunction
+    | TokenClass
+    | TokenReturn
+    | TokenNew
+    | TokenLoad
+    | TokenSemicolon --Symbols
+    | TokenComma
+    | TokenRBOpen --round brace
+    | TokenRBClose
+    | TokenCBOpen --curly brace
+    | TokenCBClose
+    | TokenSBOpen --square brackets
+    | TokenSBClose
+    | TokenAssign
+    | TokenDot
+    | TokenInt Integer
+    | TokenCompEq
+    | TokenCompNeq
+    | TokenCompLeq
+    | TokenCompLt
+    | TokenCompGeq
+    | TokenCompGt
+    | TokenArithPlus
+    | TokenArithMinus
+    | TokenArithMul
+    | TokenArithDiv
+    | TokenArithMod
+    | TokenArithInc
+    | TokenArithDec
+    | TokenLogicNot
+    | TokenLogicAnd
+    | TokenLogicOr
+    | TokenStringLit String
+    | TokenWord String
+    deriving (Show)
